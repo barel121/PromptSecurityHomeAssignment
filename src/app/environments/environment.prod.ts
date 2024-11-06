08097d55beb2bdc23fbd7bc032a1867c45e4c229
Barel <barel1@windowslive.com>
2024-11-06 13:33:11 +0200

    Added: environment files

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  anthropicAPIKey: "ANTHROPIC_API_KEY",
+  openAiAPIKey: "OPENAI_API_KEY",
+};
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  anthropicAPIKey: "ANTHROPIC_API_KEY",
+  openAiAPIKey: "OPENAI_API_KEY",
+};
